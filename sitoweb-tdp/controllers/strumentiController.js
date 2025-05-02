9caa538b9a8a00cae4675bfe7a7cd8ea63a36b10
sdanilovmiur <156649185+sdanilovmiur@users.noreply.github.com>
2025-05-02 15:03:05 +0200

    modifica pagine strumenti per integrare le nostre ap

--- sitoweb-tdp/controllers/strumentiController.js
+++ sitoweb-tdp/controllers/strumentiController.js
@@ -6,16 +6,9 @@ exports.imcForm = (req, res) => {
     res.render("strumenti/imc");
 }
 
-exports.imc = (req, res) => {
-
-}
 
 exports.CKCalForm = (req, res) => {
     res.render("strumenti/CKCal");
-}
-
-exports.CKCal = (req, res) => {
-
 }
 
 exports.timer = (req, res) => {

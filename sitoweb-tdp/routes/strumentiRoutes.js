6eb42df3ed7d6733ab7fe5befbf5f53a52958c31
Neko <53211785+N3koHunt3r@users.noreply.github.com>
2025-05-03 10:10:58 +0200

    Risolto piccolo errore

--- sitoweb-tdp/routes/strumentiRoutes.js
+++ sitoweb-tdp/routes/strumentiRoutes.js
@@ -4,9 +4,9 @@ const strumentiController = require("../controllers/strumentiController");
 
 router.get("/", strumentiController.hub);
 router.get("/imc", strumentiController.imcForm);
-router.post("/imc", strumentiController.imc);
+//router.post("/imc", strumentiController.imc);
 router.get("/CalcolaKCal", strumentiController.CKCalForm);
-router.post("/CalcolaKCal", strumentiController.CKCal);
+//router.post("/CalcolaKCal", strumentiController.CKCal);
 router.get("/timer", strumentiController.timer)
 
 module.exports = router;
